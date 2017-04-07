7a53433f65503b164199653111d76fd5b1859f35
Brian Carroll <brian.carroll.ireland@gmail.com>
2017-04-07 23:35:25 +0100

    Implement velocity alignment

--- src/State.elm
+++ src/State.elm
@@ -1,4 +1,6 @@
-module State exposing (init, update, subscriptions)
+module State exposing (..)
+
+-- module State exposing (init, update, subscriptions)
 
 import Types exposing (..)
 import Time exposing (second)
@@ -6,19 +8,17 @@ import Random exposing (generate, float, list, pair)
 import Math.Vector2 exposing (Vec2)
 
 
-t : Float
-t =
-    second / 60.0
+config =
+    { tick = second / 60.0
+    , maxWidth = 500.0
+    , numBoids = 10
+    , alignFactor = 0.01
+    }
 
 
-maxWidth : Float
-maxWidth =
-    500.0
-
-
-numBoids : Int
-numBoids =
-    50
+invNumBoids : Float
+invNumBoids =
+    1 / (toFloat config.numBoids)
 
 
 vectorGenerator : Float -> Float -> Random.Generator Vec2
@@ -32,22 +32,29 @@ boidGenerator =
     Random.map2
         (\vector1 vector2 -> { position = vector1, velocity = vector2 })
         (vectorGenerator -0.5 0.5)
-        (vectorGenerator -0.1 0.1)
+        (vectorGenerator -0.5 0.5)
 
 
 init : ( Model, Cmd Msg )
 init =
     ( []
     , generate Init <|
-        list numBoids <|
+        list config.numBoids <|
             boidGenerator
     )
+
+
+testModel : Model
+testModel =
+    [ { position = Math.Vector2.fromTuple ( 300, 400 ), velocity = Math.Vector2.fromTuple ( 0.03, 0.04 ) }
+    , { position = Math.Vector2.fromTuple ( 80, 60 ), velocity = Math.Vector2.fromTuple ( 0.08, 0.06 ) }
+    ]
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch
-        [ Time.every t (always Tick)
+        [ Time.every config.tick (always Tick)
         ]
 
 
@@ -60,15 +67,35 @@ update msg model =
             )
 
         Tick ->
-            ( List.map updateBoid model
+            ( updateBoids model
             , Cmd.none
             )
 
 
-updateBoid : Boid -> Boid
-updateBoid boid =
-    boid
-        |> bounceOffWalls
+updateBoids : List Boid -> List Boid
+updateBoids boids =
+    boids
+        |> List.map bounceOffWalls
+        |> alignBoids
+
+
+alignBoids : List Boid -> List Boid
+alignBoids boids =
+    let
+        avgVelocity =
+            Math.Vector2.scale invNumBoids <| List.foldl (\b v -> Math.Vector2.add b.velocity v) (Math.Vector2.fromTuple ( 0.0, 0.0 )) boids
+    in
+        List.map (alignBoid avgVelocity) boids
+
+
+alignBoid : Vec2 -> Boid -> Boid
+alignBoid alignDirection boid =
+    { boid
+        | velocity =
+            Math.Vector2.add boid.velocity <|
+                Math.Vector2.scale config.alignFactor <|
+                    (Math.Vector2.sub alignDirection boid.velocity)
+    }
 
 
 bounceOffWalls : Boid -> Boid
@@ -96,11 +123,11 @@ bounceOffWallsComponent : ( Float, Float ) -> ( Float, Float )
 bounceOffWallsComponent ( pos, vel ) =
     let
         pos_step =
-            pos + (t * vel)
+            pos + (config.tick * vel)
     in
-        if pos_step < -maxWidth then
-            ( -maxWidth, -vel )
-        else if pos_step > maxWidth then
-            ( maxWidth, -vel )
+        if pos_step < -config.maxWidth then
+            ( -config.maxWidth, -vel )
+        else if pos_step > config.maxWidth then
+            ( config.maxWidth, -vel )
         else
             ( pos_step, vel )
